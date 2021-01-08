a2232f3997b1ea1d9bb9d4b00db29fd6d83f567b
Rachael Savoy <rachael.a.hakim@gmail.com>
2021-01-08 22:02:20 +0900

    FEAT: added search bar.

--- src/index.jsx
+++ src/index.jsx
@@ -2,32 +2,11 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
 import '../assets/stylesheets/application.scss';
-
-class Hello extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      clicked: false
-    };
-  }
-
-  handleClick = () => {
-    this.setState({
-      clicked: !this.state.clicked
-    });
-  }
-
-  render() {
-    return(
-      <div className={this.state.clicked ? 'clicked' : null}
-      onClick={this.handleClick}>
-        Hello {this.props.name}
-      </div>)
-  }
-}
+import App from './components/app.jsx';
 
 const root = document.getElementById('root');
 if (root) {
-  ReactDOM.render(<Hello name=" Rachael" />, root);
+  ReactDOM.render(
+    <App />,
+    root);
 }
